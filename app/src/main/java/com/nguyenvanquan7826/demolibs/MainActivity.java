1648ff9db54fb26350d90c686bf8b5524de45911
nguyenvanquan7826 <nguyenvanquan7826@gmail.com>
2019-08-20 17:15:26 +0700

    init

---
+++ app/src/main/java/com/nguyenvanquan7826/demolibs/MainActivity.java
@@ -0,0 +1,29 @@
+package com.nguyenvanquan7826.demolibs;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.nguyenvanquan7826.libstoast.MToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MToast.show(MainActivity.this, "This is MToast");
+            }
+        });
+    }
+}
---
+++ libstoast/src/main/java/com/nguyenvanquan7826/libstoast/MToast.java
@@ -0,0 +1,15 @@
+package com.nguyenvanquan7826.libstoast;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Toast;
+
+public class MToast {
+    public static void show(Context context, String message) {
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        View view = toast.getView();
+        view.setBackgroundColor(Color.BLUE);
+        toast.show();
+    }
+}
